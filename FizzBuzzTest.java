d71620189eb656171c3d2c0272867f2aef87ef9d
bazegie07 <bazegie_07@yahoo.com>
2020-02-28 20:45:38 -0800

    this is my ass in java

---
+++ FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz num = new FizzBuzz();
+        String output = num.fizzBuzz(20);
+        System.out.println("FizzBuzz result = "+output);
+    }
+}
